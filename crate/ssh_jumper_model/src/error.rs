313cf3c3fd556c27b645028d0c3c4e4ed06c2b30
Azriel Hoh <azriel91@gmail.com>
2021-03-07 18:41:19 +1300

    Set `source` for each error.

--- crate/ssh_jumper_model/src/error.rs
+++ crate/ssh_jumper_model/src/error.rs
@@ -35,88 +35,63 @@ pub enum Error<'tunnel> {
 impl<'tunnel> fmt::Display for Error<'tunnel> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            Self::JumpHostConnectFail {
-                jump_host_addr,
-                io_error,
-            } => write!(
-                f,
-                "Failed to connect to Jump Host: {}, IO error: {}",
-                jump_host_addr, io_error
-            ),
-            Self::AsyncSessionInitialize(io_error) => write!(
-                f,
-                "Failed to initialize SSH session with jump host, IO error: {}",
-                io_error,
-            ),
-            Self::DnsResolverCreate(resolve_error) => {
-                write!(
-                    f,
-                    "Failed to construct DNS resolver. error: {}",
-                    resolve_error
-                )
+            Self::JumpHostConnectFail { jump_host_addr, .. } => {
+                write!(f, "Failed to connect to jump host: `{}`.", jump_host_addr)
             }
+            Self::AsyncSessionInitialize(..) => {
+                write!(f, "Failed to initialize SSH session with jump host.")
+            }
+            Self::DnsResolverCreate(..) => write!(f, "Failed to construct DNS resolver."),
             Self::DnsResolverLookup(resolve_error) => {
-                write!(f, "Failed to lookup IP for Jump Host: {}", resolve_error)
+                write!(f, "Failed to lookup IP for jump host: `{}`.", resolve_error)
             }
             Self::JumpHostIpResolutionFail { jump_host_addr } => {
                 write!(
                     f,
-                    "Failed to find IPv4 address for Jump Host: {}",
+                    "Failed to find IPv4 address for jump host: `{}`.",
                     jump_host_addr
                 )
             }
-            Self::LocalSocketAddr {
-                local_socket,
-                io_error,
-            } => write!(
+            Self::LocalSocketAddr { local_socket, .. } => write!(
                 f,
-                "Failed to retrieve socket address after binding to: {}, error: {}",
-                local_socket, io_error
+                "Failed to retrieve socket address after binding to: {}.",
+                local_socket
             ),
-            Self::LocalSocketBind {
-                local_socket,
-                io_error,
-            } => write!(
-                f,
-                "Failed to bind to local socket: {}, error: {}",
-                local_socket, io_error
-            ),
-            Self::SshHandshakeFail(io_error) => write!(
-                f,
-                "SSH handshake with jump host failed, IO error: {}",
-                io_error,
-            ),
-            Self::SshUserAuthFail(io_error) => write!(
-                f,
-                "SSH user auth with jump host failed, IO error: {}",
-                io_error,
-            ),
-            Self::SshUserAuthError(ssh2_error) => write!(
-                f,
-                "SSH user auth with jump host failed, SSH2 error: {}",
-                ssh2_error,
-            ),
+            Self::LocalSocketBind { local_socket, .. } => {
+                write!(f, "Failed to bind to local socket: {}.", local_socket)
+            }
+            Self::SshHandshakeFail(..) => write!(f, "SSH handshake with jump host failed."),
+            Self::SshUserAuthFail(..) => write!(f, "SSH user auth with jump host failed."),
+            Self::SshUserAuthError(..) => write!(f, "SSH user auth with jump host failed.",),
             Self::SshUserAuthUnknownError => {
-                write!(f, "SSH user auth with jump host failed with unknown cause.",)
+                write!(f, "SSH user auth with jump host failed with unknown cause.")
             }
-            Self::SshTunnelOpenFail(io_error) => {
-                write!(f, "Failed to open SSH tunnel, IO error: {}", io_error)
-            }
-            Self::SshTunnelListenerCreate(io_error) => write!(
-                f,
-                "Failed to spawn SSH tunnel listener, IO error: {}",
-                io_error
-            ),
-            Self::SshTunnelStreamCreate(io_error) => write!(
-                f,
-                "Failed to spawn SSH tunnel stream, IO error: {}",
-                io_error
-            ),
-            Self::SshStreamerSpawnFail(join_error) => {
-                write!(f, "Failed to join SSH tunnel streamer task: {}", join_error)
-            }
+            Self::SshTunnelOpenFail(..) => write!(f, "Failed to open SSH tunnel."),
+            Self::SshTunnelListenerCreate(..) => write!(f, "Failed to spawn SSH tunnel listener"),
+            Self::SshTunnelStreamCreate(..) => write!(f, "Failed to spawn SSH tunnel stream."),
+            Self::SshStreamerSpawnFail(..) => write!(f, "Failed to join SSH tunnel streamer task."),
         }
     }
 }
 
-impl<'tunnel> std::error::Error for Error<'tunnel> {}
+impl<'tunnel> std::error::Error for Error<'tunnel> {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            Self::JumpHostConnectFail { io_error, .. } => Some(io_error),
+            Self::AsyncSessionInitialize(io_error) => Some(io_error),
+            Self::DnsResolverCreate(resolve_error) => Some(resolve_error),
+            Self::DnsResolverLookup(resolve_error) => Some(resolve_error),
+            Self::JumpHostIpResolutionFail { .. } => None,
+            Self::LocalSocketAddr { io_error, .. } => Some(io_error),
+            Self::LocalSocketBind { io_error, .. } => Some(io_error),
+            Self::SshHandshakeFail(io_error) => Some(io_error),
+            Self::SshUserAuthFail(io_error) => Some(io_error),
+            Self::SshUserAuthError(ssh2_error) => Some(ssh2_error),
+            Self::SshUserAuthUnknownError => None,
+            Self::SshTunnelOpenFail(io_error) => Some(io_error),
+            Self::SshTunnelListenerCreate(io_error) => Some(io_error),
+            Self::SshTunnelStreamCreate(io_error) => Some(io_error),
+            Self::SshStreamerSpawnFail(join_error) => Some(join_error),
+        }
+    }
+}
